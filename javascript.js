0c7896cb029b7dddad4e08d3eaa96fb861dfbef8
Sawan Panse <panse.11@osu.edu>
2026-01-29 15:05:11 -0500

    Update index, js, working version now

--- javascript.js
+++ javascript.js
@@ -21,47 +21,72 @@ function playRound(humanChoice, computerChoice) {
 
     if (choiceHuman === "rock" && choiceComp === "scissors") {
         resultsDiv.textContent = "You win! Rock beats Scissors.";
-         humanScore++;
+        humanScore++;
+        scoreDiv.textContent = "You: " + humanScore + ", Computer: " + computerScore;
     }
     else if (choiceHuman === "rock" && choiceComp === "paper") {
         resultsDiv.textContent = "You lose! Paper beats Rock.";
         computerScore++;
+        scoreDiv.textContent = "You: " + humanScore + ", Computer: " + computerScore;
     }
     else if (choiceHuman === "paper" && choiceComp === "rock") {
         resultsDiv.textContent = "You win! Paper beats Rock.";
         humanScore++;
+        scoreDiv.textContent = "You: " + humanScore + ", Computer: " + computerScore;
     }
     else if (choiceHuman === "paper" && choiceComp === "scissors") {
         resultsDiv.textContent = "You lose! Scissors beats Paper.";
         computerScore++;
+        scoreDiv.textContent = "You: " + humanScore + ", Computer: " + computerScore;
     }
     else if (choiceHuman === "scissors" && choiceComp === "rock") {
         resultsDiv.textContent = "You lose! Rock beats Scissors.";
         computerScore++;
+        scoreDiv.textContent = "You: " + humanScore + ", Computer: " + computerScore;
     }
     else if (choiceHuman === "scissors" && choiceComp === "paper") {
         resultsDiv.textContent = "You win! Scissors beats Paper.";
         humanScore++;
+        scoreDiv.textContent = "You: " + humanScore + ", Computer: " + computerScore;
     }
     else if (choiceHuman === choiceComp) {
         resultsDiv.textContent = "Tie!";
+        scoreDiv.textContent = "You: " + humanScore + ", Computer: " + computerScore;
+    }
+
+    const winnerDiv = document.querySelector("#finalResult");
+    if (humanScore >= 5) {
+        winnerDiv.textContent = "Winner: You!";
+        disableButtons()
+    }
+    else if (computerScore >= 5) {
+        winnerDiv.textContent = "Winner: Computer!";
+        disableButtons()
     }
 }
 
-
 const resultsDiv = document.querySelector("#results");
+const scoreDiv = document.querySelector("#score");
 
-const rock = document.querySelector("#rock");
-rock.addEventListener("click", () => {
-    playRound("Rock", getComputerChoice());
-})
+// while (humanScore < 5 && computerScore < 5) {
+    const rock = document.querySelector("#rock");
+    rock.addEventListener("click", () => {
+        playRound("Rock", getComputerChoice());
+    })
 
-const paper = document.querySelector("#paper");
-paper.addEventListener("click", () => {
-    playRound("Paper", getComputerChoice());
-})
+    const paper = document.querySelector("#paper");
+    paper.addEventListener("click", () => {
+        playRound("Paper", getComputerChoice());
+    })
 
-const scissors = document.querySelector("#scissors");
-scissors.addEventListener("click", () => {
-    playRound("Scissors", getComputerChoice());
-})
+    const scissors = document.querySelector("#scissors");
+    scissors.addEventListener("click", () => {
+        playRound("Scissors", getComputerChoice());
+    })
+//}
+
+function disableButtons() {
+    document.querySelector("#rock").disabled = true;
+    document.querySelector("#paper").disabled = true;
+    document.querySelector("#scissors").disabled = true;
+}
